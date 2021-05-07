8484461ed939b2611d2c6cfa4612d757ae4bf561
Lucas Severo <lvspais2@gmail.com>
2021-05-07 19:48:22 -0300

    kafka working with reitit handlers

--- src/severo_http_connector/consumer.clj
+++ src/severo_http_connector/consumer.clj
@@ -25,7 +25,7 @@
   (let [consumer (KafkaConsumer. ^Properties @properties-consumer)]
     (.subscribe consumer [topic])
     (go-loop [records []]
-             (log/debug (str "records:" records))
+             (log/trace (str "records:" records))
              (doseq [^ConsumerRecord record records]
                (try
                  (let [{:keys [http-response-id] :as value-record} (json/parse-string (.value record) true)
@@ -33,9 +33,9 @@
                        canal-resposta (get @cache http-response-id)
                        ]
                    (>! canal-resposta value-record)
-                   (log/info "Consumed record with key %s and value %s\n" key-record value-record )
-                   (log/debug (str "cache dentro do consumer: " cache))
-                   (log/debug (str "canal-resposta: " canal-resposta))
+                   (log/info "Consumed record with key %s and value %s\n" key-record value-record)
+                   (log/trace (str "cache dentro do consumer: " cache))
+                   (log/trace (str "canal-resposta: " canal-resposta))
                    )
                  (catch Exception e nil)))
              (recur (seq (.poll consumer (Duration/ofMillis duration)))))))
--- src/severo_http_connector/core.clj
+++ src/severo_http_connector/core.clj
@@ -28,96 +28,141 @@
   (:import
     [java.util UUID]))
 
-(def app
-  (http/ring-handler
-    (http/router
-      [["/swagger.json"
-        {:get {:no-doc true
-               :swagger {:info {:title "my-api"
-                                :description "with reitit-http"}}
-               :handler (swagger/create-swagger-handler)}}]
+(defn create-generic-handler
+  [send-topic listen-topic timeout-ms poll-duration]
+  (let [canal-producer (chan)
+        cache (atom {})]
+    (consumer! send-topic cache :duration poll-duration)
+    (producer! listen-topic canal-producer)
+    (fn [{:keys [parameters] :as req}]
+      (log/debug "REQUEST: " req)
+      (let [uuid (str (UUID/randomUUID))
+            payload (assoc parameters :http-response-id uuid)
+            canal-resposta (chan)]
+        (swap! cache assoc uuid canal-resposta)
+        (log/trace "cache: " @cache)
+        (>!! canal-producer (json/generate-string payload))
+        (let [[value channel] (alts!! [canal-resposta (timeout timeout-ms)])]
+          (if value 
+            (let [{:keys [http-status]} value]
+              (log/info "RECEBEMOS O PAYLOAD " value)
+              {:status http-status
+               :headers {"Content-Type" "application/json"}
+               :body (json/generate-string (apply dissoc value [:http-status :http-response-id]))})
+            {:status 500
+             :headers {"Content-Type" "application/json"}
+             :body (json/generate-string {:message "Timeout."})}))))))
 
-       ["/math"
-        {:swagger {:tags ["math"]}}
-
-        ["/plus"
-         {:get {:summary "plus with data-spec query parameters"
-                :parameters {:query {:x int?, :y int?}}
-                :responses {200 {:body {:total pos-int?}}}
-                :handler (fn [{{{:keys [x y]} :query} :parameters}]
-                           {:status 200
-                            :body {:total (+ x y)}})}
-          :post {:summary "plus with data-spec body parameters"
-                 :parameters {:body {:x int?, :y int?}}
-                 :responses {200 {:body {:total int?}}}
-                 :handler (fn [{{{:keys [x y]} :body} :parameters}]
-                            {:status 200
-                             :body {:total (+ x y)}})}}]
-
-        ]]
-
-      {;:reitit.interceptor/transform dev/print-context-diffs ;; pretty context diffs
-       ;;:validate spec/validate ;; enable spec validation for route data
-       ;;:reitit.spec/wrap spell/closed ;; strict top-level validation
-       :exception pretty/exception
-       :data {:coercion reitit.coercion.spec/coercion
-              :muuntaja m/instance
-              :interceptors [;; swagger feature
-                             swagger/swagger-feature
-                             ;; query-params & form-params
-                             (parameters/parameters-interceptor)
-                             ;; content-negotiation
-                             (muuntaja/format-negotiate-interceptor)
-                             ;; encoding response body
-                             (muuntaja/format-response-interceptor)
-                             ;; exception handling
-                             (exception/exception-interceptor)
-                             ;; decoding request body
-                             (muuntaja/format-request-interceptor)
-                             ;; coercing response bodys
-                             (coercion/coerce-response-interceptor)
-                             ;; coercing request parameters
-                             (coercion/coerce-request-interceptor)
-                             ;; multipart
-                             (multipart/multipart-interceptor)]}})
-    (ring/routes
-      (swagger-ui/create-swagger-ui-handler
-        {:path "/"
-         :config {:validatorUrl nil
-                  :operationsSorter "alpha"}})
-      (ring/create-default-handler))
-    {:executor sieppari/executor}))
+(defn prepare-reitit-handlers
+  []
+  (into [""
+         {:swagger {:tags ["api"]}}]
+        (mapv (fn [[route method-map]]
+                [route (into {}
+                             (mapv (fn [[method {:keys [send-topic listen-topic timeout poll-duration] :as conf}]]
+                                     [method (-> conf
+                                                 (dissoc :send-topic :listen-topic :timeout :poll-duration)
+                                                 (assoc :parameters (if (#{:get :head} method)
+                                                                      {:query map?}
+                                                                      {:query map?
+                                                                       :body map?}))
+                                                 (assoc :handler (create-generic-handler send-topic listen-topic timeout poll-duration)))])
+                                   method-map))])
+              (:routes env))))
 
 (defn start-web-service 
   []
-  (jetty/run-jetty #'app {:port (-> env :http :port)
+  (let [app (http/ring-handler
+              (http/router
+                [["/swagger.json"
+                  {:get {:no-doc true
+                         :swagger {:info {:title (-> env :swagger :title)
+                                          :description (-> env :swagger :description)}}
+                         :handler (swagger/create-swagger-handler)}}]
+                 (prepare-reitit-handlers)
+                 ]
+
+                {;:reitit.interceptor/transform dev/print-context-diffs ;; pretty context diffs
+                 ;;:validate spec/validate ;; enable spec validation for route data
+                 ;;:reitit.spec/wrap spell/closed ;; strict top-level validation
+                 :exception pretty/exception
+                 :data {:coercion reitit.coercion.spec/coercion
+                        :muuntaja m/instance
+                        :interceptors [;; swagger feature
+                                       swagger/swagger-feature
+                                       ;; query-params & form-params
+                                       (parameters/parameters-interceptor)
+                                       ;; content-negotiation
+                                       (muuntaja/format-negotiate-interceptor)
+                                       ;; encoding response body
+                                       (muuntaja/format-response-interceptor)
+                                       ;; exception handling
+                                       (exception/exception-interceptor)
+                                       ;; decoding request body
+                                       (muuntaja/format-request-interceptor)
+                                       ;; coercing response bodys
+                                       (coercion/coerce-response-interceptor)
+                                       ;; coercing request parameters
+                                       (coercion/coerce-request-interceptor)
+                                       ;; multipart
+                                       (multipart/multipart-interceptor)]}})
+              (ring/routes
+                (swagger-ui/create-swagger-ui-handler
+                  {:path "/"
+                   :config {:validatorUrl nil
+                            :operationsSorter "alpha"}})
+                (ring/create-default-handler))
+              {:executor sieppari/executor})]
+    (jetty/run-jetty app {:port (or (-> env :http :port) 3000)
                           :join? true
                           :min-threads (or (-> env :http :min-threads) 16)
                           :max-threads (or (-> env :http :max-threads) 100)
                           :max-idle-time (or (-> env :http :max-idle-time) (* 60 1000 30))
                           :request-header-size (or (-> env :http :request-header-size) 8192)
-                          }))
+                          })))
+
+
+
+
+
+(comment 
+  {:send-topic "poc"
+   :listen-topic "poc"
+   :timeout 2000
+   :poll-duration 100;milliseconds
+   } 
+  (:routes env)
+  (flatten 
+    (into []
+          (mapv (fn [route method-map]
+                  (mapv (fn [method {:keys [send-topic listen-topic timeout poll-duration] :as conf}]
+                          (let [reitit-map (-> conf
+                                               (dissoc :send-topic :listen-topc :timeout :poll-duration)
+                                               (assoc :handler (create-generic-handler send-topic listen-topic timeout-ms poll-duration)))]
+                            retit-map))
+                        method-map))
+                (:routes env))))
+  )
 
 (def canal-producer (chan))
 (def cache (atom {}))
 
 (defn -main [& args]
-  (log/info "criando consumidor")
-  (consumer! "poc" cache)
-  (log/info "criando produtor")
-  (producer! "poc" canal-producer)
+  ;(log/info "criando consumidor")
+  ;(consumer! "poc" cache)
+  ;(log/info "criando produtor")
+  ;(producer! "poc" canal-producer)
 
-  ;;handler http
-  (let [uuid (str (UUID/randomUUID))
-        payload {:http-response-id uuid :payload "lol"}
-        canal-resposta (chan)]
-    (swap! cache assoc uuid canal-resposta)
-    (log/debug "cache: " @cache)
-    (>!! canal-producer (json/generate-string payload))
-    (let [[value channel] (alts!! [canal-resposta (timeout 2000)])]
-      (if value 
-        (log/info "RECEBEMOS O PAYLOAD " value)
-        (log/info "Falha."))))
-
-
+  ;handler http
+  ;(let [uuid (str (UUID/randomUUID))
+        ;payload {:http-response-id uuid :payload "lol"}
+        ;canal-resposta (chan)]
+    ;(swap! cache assoc uuid canal-resposta)
+    ;(log/trace "cache: " @cache)
+    ;(>!! canal-producer (json/generate-string payload))
+    ;(let [[value channel] (alts!! [canal-resposta (timeout 2000)])]
+      ;(if value 
+        ;(log/info "RECEBEMOS O PAYLOAD " value)
+        ;(log/info "Falha."))))
+  (start-web-service)
   ) 
