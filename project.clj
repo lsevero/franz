46684ea5bede71f1bda38fb0269e3e0ac00cb795
Lucas Severo <lvspais2@gmail.com>
2021-05-07 12:18:22 -0300

    added log4j2 logs and added a config.edn

--- project.clj
+++ project.clj
@@ -5,8 +5,23 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [cheshire "5.8.1"]
+                 [yogthos/config "1.1.4"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [metosin/reitit "0.4.2"]
                  [org.apache.kafka/kafka-clients "2.1.0"]
                  [org.clojure/core.async "1.3.618"]
+                 [org.clojure/tools.logging "1.1.0"]
+                 [org.apache.logging.log4j/log4j-api "2.13.3"]
+                 [org.apache.logging.log4j/log4j-core "2.13.3"]
+                 [org.apache.logging.log4j/log4j-slf4j-impl "2.13.3"]
                  ]
   :main severo-http-connector.core/-main
+  :profiles {:dev {:global-vars {*warn-on-reflection* true}
+                   :plugins [[cider/cider-nrepl "0.25.4"]]
+                   :resource-paths ["resources"
+                                    "example-config"
+                                    ]
+                   }
+             :uberjar {:aot :all}
+             } 
   :repl-options {:init-ns severo-http-connector.core})
--- src/severo_http_connector/core.clj
+++ src/severo_http_connector/core.clj
@@ -1,105 +1,114 @@
 (ns severo-http-connector.core
   (:gen-class)
   (:require
-   [cheshire.core :as json]
-   [clojure.java.io :as jio]
-   [clojure.core.async
-    :as a
-    :refer [>! <! >!! <!! go chan buffer close! thread
-            alts! alts!! timeout]])
+    [cheshire.core :as json]
+    [clojure.java.io :as jio]
+    [clojure.core.async
+     :as a
+     :refer [>! <! >!! <!! go chan buffer close! thread go-loop
+             alts! alts!! timeout]]
+    [clojure.tools.logging :as log] 
+    [config.core :refer [env]]
+    )
   (:import
-   (java.util Properties)
-   (java.time Duration)
-   (org.apache.kafka.clients.consumer ConsumerConfig KafkaConsumer)
-   (org.apache.kafka.clients.admin AdminClient NewTopic)
-   (org.apache.kafka.clients.producer Callback KafkaProducer ProducerConfig ProducerRecord)
-   (org.apache.kafka.common.errors TopicExistsException)))
+    [java.util Properties UUID]
+    [java.time Duration]
+    [org.apache.kafka.clients.consumer ConsumerConfig KafkaConsumer ConsumerRecord]
+    [org.apache.kafka.clients.admin AdminClient NewTopic]
+    [org.apache.kafka.clients.producer Callback KafkaProducer ProducerConfig ProducerRecord RecordMetadata]
+    [org.apache.kafka.common.errors TopicExistsException]))
 
 
-(defn- build-properties-producer []
-  (with-open [config (-> "kafka.config" jio/resource jio/reader)]
-    (doto (Properties.)
-      (.putAll {ProducerConfig/KEY_SERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringSerializer"
-                ProducerConfig/VALUE_SERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringSerializer"})
-      (.load config))))
 
-(defn- create-topic! [topic partitions replication cloud-config]
-  (let [ac (AdminClient/create cloud-config)]
-    (try
-      (.createTopics ac [(NewTopic. topic partitions replication)])
-      ;; Ignore TopicExistsException, which would get thrown if the topic was previously created
-      (catch TopicExistsException e nil)
-      (finally
-        (.close ac)))))
+(def ^Properties properties-consumer
+  (delay (doto (Properties.)
+           (.putAll {ConsumerConfig/GROUP_ID_CONFIG, "clojure_example_group"
+                     ConsumerConfig/KEY_DESERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringDeserializer"
+                     ConsumerConfig/VALUE_DESERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringDeserializer"
+                     ConsumerConfig/BOOTSTRAP_SERVERS_CONFIG ^String (:kafka env)}))))
 
-(defn- build-properties-consumer []
-  (with-open [config (-> "kafka.config" jio/resource jio/reader)]
-    (doto (Properties.)
-      (.putAll {ConsumerConfig/GROUP_ID_CONFIG, "clojure_example_group"
-                ConsumerConfig/KEY_DESERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringDeserializer"
-                ConsumerConfig/VALUE_DESERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringDeserializer"})
-      (.load config))))
+(def ^Properties properties-producer
+  (delay (doto (Properties.)
+           (.putAll {ProducerConfig/KEY_SERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringSerializer"
+                     ProducerConfig/VALUE_SERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringSerializer"
+                     ProducerConfig/BOOTSTRAP_SERVERS_CONFIG ^String (:kafka env)}))))
 
-(defn producer! [topic]
-  (let [props (build-properties-producer)
-        print-ex (comp println (partial str "Failed to deliver message: "))
-        print-metadata #(printf "Produced record to topic %s partition [%d] @ offest %d\n"
-                                (.topic %)
-                                (.partition %)
-                                (.offset %))
-        create-msg #(let [k "alice"
-                          v (json/generate-string {:count %})]
-                      (printf "Producing record: %s\t%s\n" k v)
-                      (ProducerRecord. topic k v))]
-    (with-open [producer (KafkaProducer. props)]
-      (create-topic! topic 1 3 props)
-      (let [;; We can use callbacks to handle the result of a send, like this:
-            callback (reify Callback
-                       (onCompletion [this metadata exception]
-                         (if exception
-                           (print-ex exception)
-                           (print-metadata metadata))))]
-        (doseq [i (range 5)]
-          (.send producer (create-msg i) callback))
-        (.flush producer)
-        ;; Or we could wait for the returned futures to resolve, like this:
-        (let [futures (doall (map #(.send producer (create-msg %)) (range 5 10)))]
-          (.flush producer)
-          (while (not-every? future-done? futures)
-            (Thread/sleep 50))
-          (doseq [fut futures]
-            (try
-              (let [metadata (deref fut)]
-                (print-metadata metadata))
-              (catch Exception e
-                (print-ex e))))))
-      (printf "10 messages were produced to topic %s!\n" topic))))
+(def canal-producer (chan))
+(def cache (atom {}))
 
-(defn consumer! [topic]
-  (with-open [consumer (KafkaConsumer. (build-properties-consumer))]
+(defn producer! [topic canal-producer]
+  (letfn [(create-topic! [topic partitions replication ^Properties cloud-config]
+            (let [ac (AdminClient/create cloud-config)]
+              (try
+                (.createTopics ac [(NewTopic. topic partitions replication)])
+                ;; Ignore TopicExistsException, which would get thrown if the topic was previously created
+                (catch TopicExistsException e nil)
+                (finally
+                  (.close ac)))))]
+    (let [print-ex #(log/error % "Failed to deliver message.")
+          print-metadata (fn [^RecordMetadata x]
+                           (log/info (format "Produced record to topic %s partition [%d] @ offest %d\n"
+                                             (.topic x)
+                                             (.partition x)
+                                             (.offset x))))
+          producer (KafkaProducer. @properties-producer)
+          callback (reify Callback
+                     (onCompletion [this metadata exception]
+                       (if exception
+                         (print-ex exception)
+                         (print-metadata metadata)))) 
+          ]
+      (create-topic! topic 1 3 @properties-producer)
+      (go-loop []
+               (let [record (ProducerRecord. topic (<! canal-producer))]
+                 (log/debug "canal-producer recebeu " record)
+                 (doto producer 
+                   (.send record callback)
+                   (.flush)))
+               (recur))
+
+      )))
+
+(defn consumer! [topic cache]
+  (let [consumer (KafkaConsumer. @properties-consumer)]
     (.subscribe consumer [topic])
-    (loop [tc 0
-           records []]
-      (let [new-tc (reduce
-                    (fn [tc record]
-                      (let [value (.value record)
-                            cnt (get (json/parse-string value) "count")
-                            new-tc (+ tc cnt)]
-                        (printf "Consumed record with key %s and value %s, and updated total count to %d\n"
-                                (.key record)
-                                value
-                                new-tc)
-                        new-tc))
-                        tc
-                        records)]
-        (println "Waiting for message in KafkaConsumer.poll")
-        (recur new-tc
-               (seq (.poll consumer (Duration/ofSeconds 1))))))))
+    (go-loop [records []]
+             (log/debug (str "records:" records))
+             (doseq [^ConsumerRecord record records]
+               (try
+                 (let [{:keys [http-response-id] :as value-record} (json/parse-string (.value record) true)
+                       key-record (.key record)
+                       canal-resposta (get @cache http-response-id)
+                       ]
+                   (>! canal-resposta value-record)
+                   (log/info "Consumed record with key %s and value %s\n" key-record value-record )
+                   (log/debug (str "cache dentro do consumer: " cache))
+                   (log/debug (str "canal-resposta: " canal-resposta))
+                   )
+                 (catch Exception e nil)))
+             (recur (seq (.poll consumer (Duration/ofSeconds 1)))))))
 
-(defn -main [topic & args]
-  (println "criando consumidor")
-  (.start (Thread. (partial consumer! topic)))
-  (Thread/sleep 3000)
-  (println "criando publisher")
-  (.start (Thread. (partial producer! topic)))
-  )
+
+
+(defn -main [& args]
+  (log/info "criando consumidor")
+  (consumer! "poc" cache)
+  (log/info "criando produtor")
+  (producer! "poc" canal-producer)
+  (Thread/sleep 1000)
+
+  ;;handler http
+  (let [uuid (str (UUID/randomUUID))
+        payload {:http-response-id uuid :payload "lol" }
+        canal-resposta (chan)]
+    (swap! cache assoc uuid canal-resposta)
+    (log/debug "cache: " @cache)
+    (>!! canal-producer (json/generate-string payload))
+    (Thread/sleep 500)
+    (let [[value channel] (alts!! [canal-resposta (timeout 2000)])]
+      (if value 
+        (log/info "RECEBEMOS O PAYLOAD " value)
+        (log/info "Falha."))))
+
+
+  ) 
