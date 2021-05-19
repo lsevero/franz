99302c65ed1821ff94eac010f1546b7920591d22
Lucas Severo <lvspais2@gmail.com>
2021-05-19 12:56:13 -0300

    incrementing project version to 1.4.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject franz "1.4.0"
+(defproject franz "1.4.1"
   :description "A http to kafka translator"
   :url "https://github.com/lsevero/franz"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
