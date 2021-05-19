3e41964b71b57565b07df9b03763a19c0f86cd89
Lucas Severo <lvspais2@gmail.com>
2021-05-19 13:04:56 -0300

    changed project description

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject franz "1.4.1"
-  :description "A http to kafka translator"
+  :description "A http to kafka gateway"
   :url "https://github.com/lsevero/franz"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
